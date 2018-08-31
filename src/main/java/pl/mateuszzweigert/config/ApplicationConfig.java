1070e4370d2a0d7b4510365dda55ff27c91bb197
mzweigert <mateuszzweigert@gmail.com>
2018-08-31 15:00:57 +0200

    Migrate to https protocol

--- src/main/java/pl/mateuszzweigert/config/ApplicationConfig.java
+++ src/main/java/pl/mateuszzweigert/config/ApplicationConfig.java
@@ -1,6 +1,11 @@
 package pl.mateuszzweigert.config;
 
+import org.apache.catalina.Context;
+import org.apache.catalina.connector.Connector;
 import org.apache.coyote.http2.Http2Protocol;
+import org.apache.tomcat.util.descriptor.web.SecurityCollection;
+import org.apache.tomcat.util.descriptor.web.SecurityConstraint;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
@@ -17,19 +22,43 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 @ComponentScan(basePackageClasses = Application.class)
 class ApplicationConfig {
 
+    @Value("${server.port.http}")
+    private int serverPortHttp;
+
+    @Value("${server.port}")
+    private int serverPortHttps;
+
     @Bean
     public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
         return new PropertySourcesPlaceholderConfigurer();
     }
 
     @Bean
-    public ServletWebServerFactory servletWebServerFactory(){
-        TomcatServletWebServerFactory factory = new TomcatServletWebServerFactory();
-        factory.addConnectorCustomizers((connector -> {
-            connector.addUpgradeProtocol(new Http2Protocol());
-            connector.setPort(80);
-        }));
-        return factory;
+    public ServletWebServerFactory servletContainer() {
+        TomcatServletWebServerFactory tomcat =
+                new TomcatServletWebServerFactory() {
+
+                    @Override
+                    protected void postProcessContext(Context context) {
+                        SecurityConstraint securityConstraint = new SecurityConstraint();
+                        securityConstraint.setUserConstraint("CONFIDENTIAL");
+                        SecurityCollection collection = new SecurityCollection();
+                        collection.addPattern("/*");
+                        securityConstraint.addCollection(collection);
+                        context.addConstraint(securityConstraint);
+                    }
+                };
+        tomcat.addAdditionalTomcatConnectors(createHttpConnector());
+        return tomcat;
     }
 
+    private Connector createHttpConnector() {
+        Connector connector =
+                new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setScheme("http");
+        connector.setSecure(false);
+        connector.setPort(serverPortHttp);
+        connector.setRedirectPort(serverPortHttps);
+        return connector;
+    }
 }
