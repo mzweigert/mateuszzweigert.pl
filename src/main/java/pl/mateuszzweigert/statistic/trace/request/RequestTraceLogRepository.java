4aee18f7fbc4153b38be29a641b5c864814af32b
mzweigert <mateuszzweigert@gmail.com>
2018-10-15 19:48:36 +0200

    Fix finding and removing duplicated request trace logs.

--- src/main/java/pl/mateuszzweigert/statistic/trace/request/RequestTraceLogRepository.java
+++ src/main/java/pl/mateuszzweigert/statistic/trace/request/RequestTraceLogRepository.java
@@ -3,7 +3,9 @@ package pl.mateuszzweigert.statistic.trace.request;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpMethod;
 
+import java.util.List;
+
 interface RequestTraceLogRepository extends JpaRepository<RequestTraceLog, String> {
 
-    RequestTraceLog findByRequestUriAndMethod(String requestURI, HttpMethod method);
+    List<RequestTraceLog> findByRequestUriAndMethod(String requestURI, HttpMethod method);
 }
